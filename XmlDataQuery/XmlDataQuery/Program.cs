dad4d63ad2e9e3953e1c71ca754f9f497d63c7cf
Michael Miles <boat081062@gmail.com>
2016-05-24 05:13:42 -0500

    xml Query worked like a champ. As soon as I got all the silly coding errors out of the way.

--- XmlDataQuery/XmlDataQuery/Program.cs
+++ XmlDataQuery/XmlDataQuery/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace XmlDataQuery
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlDataQuery
 {
     class Program
     {
         static void Main()
         {
+            #region SurfaceBook
+            //var customers = XDocument.Load(@"f:\XML_Files\Customers.xml"); 
+            #endregion
 
+            #region SurfacePro 3
+            var customers = XDocument.Load(@"d:\XML_Files\Customers.xml"); 
+            #endregion
+
+            var xml = from x in customers.Descendants("Customer")
+                where x.Attribute("City").Value == "Mexico D.F."
+                select x;
+
+            foreach(var name in xml)
+                Console.WriteLine($"Customer Name: {name.Attribute("ContactName").Value}");
         }
     }
 }
