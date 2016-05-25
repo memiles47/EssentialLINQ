ee97d709aa39956c9796b70590dcd1a11cfe23f7
Michael E Miles <michael@miles-me.net>
2016-05-25 03:09:34 -0500

    Comment deletions - not needed

--- DatabaseQuery/DatabaseQuery/Program.cs
+++ DatabaseQuery/DatabaseQuery/Program.cs
@@ -17,7 +17,6 @@ namespace DatabaseQuery
     {
         static void Main()
         {
-            //Additions making this code machine agnostic
             var connectionString = Environment.MachineName == "MEM-SURFACEBOOK"
                 ? @"F:\SQL_DataBaseFiles\myDbase.mdf"
                 : @"D:\SQL_DataBaseFiles\myDbase.mdf";
