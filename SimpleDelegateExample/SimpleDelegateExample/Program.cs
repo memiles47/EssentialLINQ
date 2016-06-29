bd1c1570b9e43c4d9ee9eb0305d68ad59cbba466
micha <micha@MEM-SURFACEPRO3>
2016-06-29 05:43:12 -0500

    Move project to Essential LINQ repository.

---
+++ SimpleDelegateExample/SimpleDelegateExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleDelegateExample
+{
+    internal class Program
+    {
+        //public delegate int MyDelegate(int a, int b);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void CallDelegate(Func<int, int, int> myDelegate)
+        {
+            Console.WriteLine($"{myDelegate(272, 153)}\n");
+        }
+
+        private static void Main()
+        {
+            Func<int, int, int> myDelegate = Add;
+
+            Console.WriteLine($"{myDelegate(271, 152)}");
+
+            CallDelegate(myDelegate);
+
+            Func<string, int, int, string> showMe = (a, b, c) => string.Format(a, b, c, (b + c));
+            Console.WriteLine($"Result of calling the \'ShowMe\' Function: {showMe("{0} + {1} = {2}", 3, 5)}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
