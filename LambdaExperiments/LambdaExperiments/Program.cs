53fee5597edbc846b99af0b900cdb60c07804356
Michael E Miles <michael@miles-me.net>
2016-07-13 10:59:23 -0500

    Added Lambda experiments for my testing

---
+++ LambdaExperiments/LambdaExperiments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LambdaExperiments
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Using a method
+            Console.WriteLine($"SimpleMath Method: {SimpleMath1(1, 2, 3)}");
+
+            //Lambda
+            Func<int, int, int, int> simpleMath2 = (a, b, c) => (a + b)/c;
+            Console.WriteLine($"SimpleMath Lambda: {simpleMath2(1, 2, 3)}");
+
+            Console.ReadLine();
+        }
+
+        static int SimpleMath1(int a, int b, int c)
+        {
+            return (a + b)/c;
+        }
+    }
+}
