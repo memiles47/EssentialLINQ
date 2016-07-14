fc7ae66aa9feca31cb1f41bd89cd9ae7fd697d58
Michael E Miles <michael@miles-me.net>
2016-07-14 05:59:18 -0500

    Essential LINQ Lambda exercises

--- LambdaExamples/LambdaExamples/Program.cs
+++ LambdaExamples/LambdaExamples/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LambdaExamples
+﻿using System;
+
+namespace LambdaExamples
 {
     class Program
     {
         static void Main()
         {
+            Console.WriteLine($"Subtract Method: {Subtract(5, 2)}");
 
+            Func<int, int, int> subtract = (a, b) => a - b;
+            Console.WriteLine($"subtract Lambda: {subtract(5, 2)}");
+
+            Console.ReadLine();
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
         }
     }
 }
