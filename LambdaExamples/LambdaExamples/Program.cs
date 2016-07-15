bc3b99b3649705efeb460d7ae0e1e5cd86b3a937
Michael E Miles <michael@miles-me.net>
2016-07-15 07:00:37 -0500

    Multiply Method to lambda

--- LambdaExamples/LambdaExamples/Program.cs
+++ LambdaExamples/LambdaExamples/Program.cs
@@ -2,21 +2,34 @@
 
 namespace LambdaExamples
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
+            #region Subtract Method
             Console.WriteLine($"Subtract Method: {Subtract(5, 2)}");
 
             Func<int, int, int> subtract = (a, b) => a - b;
-            Console.WriteLine($"subtract Lambda: {subtract(5, 2)}");
+            Console.WriteLine($"subtract Lambda: {subtract(5, 2)}\n");
+            #endregion
+            #region Multiply Method
+            Console.WriteLine($"Multiply Method: {Multiply(5)}");
+
+            Func<int, int> multiply = a => a * 5;
+            Console.WriteLine($"Multiply Lambda: {multiply(5)}\n");
+            #endregion
 
             Console.ReadLine();
         }
 
-        static int Subtract(int a, int b)
+        public static int Subtract(int a, int b)
         {
             return a - b;
+        }
+
+        public static int Multiply(int a)
+        {
+            return a*5;
         }
     }
 }
