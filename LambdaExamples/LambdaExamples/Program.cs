a031d5c84318e9f47ab66310811b17a672be8672
Michael E Miles <michael@miles-me.net>
2016-07-15 10:54:36 -0500

    Sample Methods and equivalent Lambda expressions

--- LambdaExamples/LambdaExamples/Program.cs
+++ LambdaExamples/LambdaExamples/Program.cs
@@ -12,11 +12,34 @@ namespace LambdaExamples
             Func<int, int, int> subtract = (a, b) => a - b;
             Console.WriteLine($"subtract Lambda: {subtract(5, 2)}\n");
             #endregion
+
             #region Multiply Method
             Console.WriteLine($"Multiply Method: {Multiply(5)}");
 
             Func<int, int> multiply = a => a * 5;
-            Console.WriteLine($"Multiply Lambda: {multiply(5)}\n");
+            Console.WriteLine($"multiply Lambda: {multiply(5)}\n");
+            #endregion
+
+            #region Display Method
+            Display("Michael Miles");
+
+            Action<string> display = a => Console.WriteLine($"display Lambda: {a}");
+            display("Michael Miles\n");
+
+            #endregion
+
+            #region DisplayWarning Method
+            DisplayWarning();
+
+            Action displayWarning = () => Console.WriteLine("displayWarning Lambda: Warning\n");
+            displayWarning();
+            #endregion
+
+            #region SimpleMath Method
+            Console.WriteLine($"SimpleMath Method: {SimpleMath(1, 3, 3)}");
+
+            Func<int, int, int, decimal> simpleMath = (a, b, c) => (a + b) / c;
+            Console.WriteLine($"simpleMath Lambda: {simpleMath(1, 2, 3)}\n");
             #endregion
 
             Console.ReadLine();
@@ -30,6 +53,21 @@ namespace LambdaExamples
         public static int Multiply(int a)
         {
             return a*5;
+        }
+
+        public static void Display(string value)
+        {
+            Console.WriteLine($"Display Method: {value}");
+        }
+
+        public static void DisplayWarning()
+        {
+            Console.WriteLine("DisplayWarning Method: Warning");
+        }
+
+        public static decimal SimpleMath(int a, int b, int c)
+        {
+            return (a + b)/c;
         }
     }
 }
